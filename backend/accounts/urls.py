180058c580b73f9a44cc2cae4dc5f9367e0dd2f5
codeOlam <frankuchenna30@gmail.com>
2022-01-12 09:26:58 +0100

    removed on used route defination in accounts urls file

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,5 +5,4 @@ from .api.views import RegisterApiView, VerifyEmailApiView
 urlpatterns = [
     path('register/', RegisterApiView.as_view(), name="register"),
     path('email-verify/', VerifyEmailApiView.as_view(), name="email-verify"),
-    # path('email-verify/<token>/', VerifyEmailApiView.as_view(), name='email_activation'),
 ]
