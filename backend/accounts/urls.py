807d603e7bd87ed6683638159a10f01f84e9dd87
codeOlam <frankuchenna30@gmail.com>
2022-01-20 11:30:32 +0100

    added refresh token api

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from .api.views import RegisterApiView, VerifyEmailApiView, ResendVerifyEmailViewApi, LoginApiView
 
 urlpatterns = [
     path('register/', RegisterApiView.as_view(), name='register'),
     path('email-verify/', VerifyEmailApiView.as_view(), name='email-verify'),
     path('resend-link/', ResendVerifyEmailViewApi.as_view(), name='resend-link'),
-    path('login/', LoginApiView.as_view(), name='login')
+    path('login/', LoginApiView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
