62c8eed200cdbe8c6bd8f0cf860865c592e917f7
codeOlam <frankuchenna30@gmail.com>
2022-01-21 19:52:09 +0100

    set up sass and scss styles for app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
+import styles from './app.module.scss';
+
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Django and React app with Typescript</h1>
     </div>
   );
